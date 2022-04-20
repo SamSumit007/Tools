527302c7dd978cbe985f684d84fea51231af25b3
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-04-20 23:07:24 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,62 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-import { Counter } from './components/Counter';
-
-
+import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [show, setShow] = useState(true);
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (timer) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10);
+      }, 10)
+    }
+    else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timer])
+
 
   return (
     <div className="App">
-   {show ? <Counter/> : null}
-   <button onClick={()=>{
-     setShow(!show)
-   }}>{show ? 'Hide' : 'Show'} Timer </button>
+      <div>
+        <span>{("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 100) % 100)).slice(-2)}</span>
+
+      </div>
+
+      <div>
+        {!timer && time === 0 && (
+          <button onClick={() => setTimer(true)}>Start</button>
+
+        )}
+
+        {timer && (
+          <button onClick={() => setTimer(false)}>Stop</button>
+
+        )}
+
+        {!timer && time !== 0 && (
+          <button onClick={() => setTimer(true)}>Resume</button>
+
+        )}
+
+        {!timer && time > 0 && (
+          <button onClick={() => setTime(0)}>Reset</button>
+
+        )}
+
+      </div>
 
     </div>
   );
-  }
-export default App
+}
+
+export default App;
